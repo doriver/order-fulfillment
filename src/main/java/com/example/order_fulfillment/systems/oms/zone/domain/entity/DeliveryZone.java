e89ba696dc7643134283d010286adf7f0192b52c
DESKTOP-I6L47K7\user <leesang@gro>
2026-06-11 16:11:03 +0900

    feat(oms): 주문 배송지와 매핑될 권역entity

---
+++ src/main/java/com/example/order_fulfillment/systems/oms/zone/domain/entity/DeliveryZone.java
@@ -0,0 +1,38 @@
+package com.example.order_fulfillment.systems.oms.zone.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DeliveryZone {
+    @Id
+    @Column(nullable = false, length = 20)
+    private String code; // 권역 코드
+
+    @Column(nullable = false, length = 100)
+    private String name; // 권역명
+
+    @Column(nullable = false, length = 100)
+    private String region; // 해당 지역
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @NotNull
+    private RegionType regionType;
+
+    @Column(columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP",
+            insertable = false, updatable = false // DB가 직접 입력하므로 ,JPA는 신경 끄라는 의미
+    )
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/example/order_fulfillment/systems/oms/zone/domain/entity/RegionType.java
@@ -0,0 +1,6 @@
+package com.example.order_fulfillment.systems.oms.zone.domain.entity;
+
+public enum RegionType {
+    CITY,
+    GU
+}
